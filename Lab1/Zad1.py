8eda9e8685f2a86f39a7e937f913ed80359b8d88
Marcin Serafin <Marcin.serafin0325@gmail.com>
2024-03-03 20:56:22 +0100

    no dobra jest commit

--- Lab1/Zad1.py
+++ Lab1/Zad1.py
@@ -18,16 +18,13 @@ def f_prime_prime_prime(x):
     return 2*(1/np.cos(x)**2)**2 + 2*np.tan(x)*(-2*np.tan(x)/np.cos(x)**2)
 
 def forward_diff(x,h,f):
-    return (f(x+h)-f(x))/h
+    return (f(x + h) - f(x)) / h
 
 def central_diff(x,h,f):
     return (f(x+h)-f(x-h))/(2*h)
 
 def h_value():
-    h_values=[]
-    for i in range(0,17):
-        h_values.append(10**(-i))
-    return h_values
+    return 1/10**np.arange(0, 17)
 
 def h_min_forward():
     return 2*np.sqrt(epsilon)/abs(f_prime_prime(1))
@@ -38,22 +35,20 @@ def h_min_central():
 def errors(f,M,method):
     h_min=1
     h_values = h_value()
-    num_diff_error = []
-    rou_diff_error  =[]
-    tru_diff_error = []
-    for h in h_values:
-        num_diff_error.append(abs(method(1,h,f)-f_prime(1)))
-        rou_diff_error.append(2*epsilon/h)
-        tru_diff_error.append(M/6*h**2)
-        if(h_min>abs(method(1,h,f)-f_prime(1))):
-            h_min=abs(method(1,h,f)-f_prime(1))
+    values = [method(1,h,f) for h in h_values]
+    num_diff_error=abs(values - f_prime(1))
+    if(method==forward_diff):
+        rou_diff_error=[2*epsilon/h for h in h_values]
+        tru_diff_error=[M*h/6 for h in h_values]
+    else:
+        rou_diff_error=[epsilon/h for h in h_values]
+        tru_diff_error=[(M*h**2)/6 for h in h_values]
     return num_diff_error,rou_diff_error,tru_diff_error,h_min
 
 def main(f):
     h_values = h_value()
     num_diff_error, rou_diff_error, tru_diff_error,h_min = errors(f, abs(f_prime_prime(1)), forward_diff)
     num_diff_error2, rou_diff_error2, tru_diff_error2 ,h_min2= errors(f, abs(f_prime_prime_prime(1)), central_diff)
-    
     plt.figure(figsize=(16, 6))
     plt.subplot(1, 2, 1)
     plt.plot(h_values, num_diff_error, label='Approximation Error', marker='o')
@@ -73,7 +68,6 @@ def main(f):
     plt.ylabel('Absolute Error')
     plt.title('Central Difference')
     plt.legend()
-    
     plt.tight_layout()
     plt.show()
 
