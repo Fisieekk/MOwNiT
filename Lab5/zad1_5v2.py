15910cfacaea5eb365fd600edf2a35812df7d109
Marcin Serafin <Marcin.serafin0325@gmail.com>
2024-04-10 12:18:30 +0200

    Second version finished and jupyter added

--- Lab5/zad1_5v2.py
+++ Lab5/zad1_5v2.py
@@ -5,19 +5,22 @@ from numpy.polynomial.polynomial import Polynomial
 # dane
 years= np.array([1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980])
 population = np.array([76212168, 92228496, 106021537, 123202624, 132164569, 151325798, 179323175, 203302031, 226542199])    
-
-def build_matrix(m,n,x):
-    A = np.zeros((m,n+1))
-    for i in range(m):
-        for j in range(n+1):
-            A[i][j] = x[i]**j 
+np.set_printoptions(formatter={'float': "\t{: 0.0f}\t".format})
+def build_matrix(x, m):
+    n = len(x)
+    A = np.zeros((n, m+1))
+    for i in range(n):
+        for j in range(m+1):
+            A[i][j] = x[i]**j
+    if m==6:
+        print(A)
     return A
 
-
-def approx(x,y,n):
-    m = len(x)
-    A = build_matrix(m,n,x)
-    c=(inv(A.T@A)@A.T)@y
+def approx(x, y, m):
+    A = build_matrix(x, m)
+    c= inv(A.T @ A) @ A.T @ y
+    if m==6:
+        print(c)
     return c
 
 def extrapolate(c,x):
@@ -34,14 +37,14 @@ def optimize(x, y, m):
     y_hat = extrapolate(p, 1990)
     return relative_error(248709873, y_hat)
 
-n = np.arange(7)
-errors = np.array([optimize(years, population, i) for i in n])
-plt.plot(n, errors,'o', label='Błąd względny')
+m = np.arange(7)
+errors = np.array([optimize(years, population, i) for i in m])
+plt.plot(m, errors,'o', label='Błąd względny')
 plt.xlabel('Stopień')
 plt.ylabel('Błąd względny')
 plt.legend()
 plt.show()
-best_m = n[np.argmin(errors)]
+best_m = m[np.argmin(errors)]
 print(f'Najlepszy stopień wielomianu: {best_m}')
 print(f'Błąd względny dla najlepszego stopnia wielomianu: {round(errors[best_m]*100,2)}%')
 
@@ -70,8 +73,8 @@ def AICc(y, y_hat, k):
 
 # kryterium informacyjne Akaikego z korektą
 
-aic_c = np.array([AICc(population, extrapolate(approx(years, population, i), years), i) for i in n])
-best_aic_c = n[np.argmin(aic_c)]
+aic_c = np.array([AICc(population, extrapolate(approx(years, population, i), years), i) for i in m])
+best_aic_c = m[np.argmin(aic_c)]
 print(aic_c)
 print(f'Najlepszy stopień wielomianu wg AICc: {best_aic_c}')
 print(f'Wartość kryterium informacyjnego Akaikego z korektą dla najlepszego stopnia wielomianu: {aic_c[best_aic_c]}')
