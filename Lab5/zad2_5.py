f1265c3e7b99966ab9bd9456d43ebb746e021b1b
Marcin Serafin <Marcin.serafin0325@gmail.com>
2024-04-11 08:27:53 +0200

    Update zad2_5.py

--- Lab5/zad2_5.py
+++ Lab5/zad2_5.py
@@ -1,24 +1,53 @@
+# Wykonaj aproksymację średniokwadratową ciągłą funkcji f(x) =
+# √
+# x w przedziale [0,2] wielomianem drugiego stopnia, używając wielomianów
+# Czebyszewa. Aproksymacja ta jest tańszym obliczeniowo zamiennikiem aproksymacji jednostajnej.
+
+
 import numpy as np
 import matplotlib.pyplot as plt 
-from numpy.polynomial.chebyshev import Chebyshev
+from scipy.integrate import quad
 
 # dane
-x = np.linspace(0, 2, 100)
-y = np.sqrt(x)
 
-# funkcja aproksymująca
-def approx(x, y, m):
-    p = Chebyshev.fit(x, y, m)
+def f(x):
+    return np.sqrt(x)
+def T(k,x):
+    x=x-1
+    return np.cos(k*np.arccos(x))
+def w(x):
+    t=x-1
+    return (1-t**2)**(-1/2)
+def phi(k):
+    return np.pi if k==0 else np.pi/2
+def integrand(x,k):
+    return f(x)*T(k,x)*w(x)
+def c(k):
+    c, _ = quad(integrand, 0, 2, args=(k,))
+    print(c)
+    c=c/phi(k)
+    return c
+def p_gwiazdka(m):
+    ck=[]
+    phik=[]
+    for k in range(m+1):
+        ck.append(c(k))
+        phik.append(lambda x: T(k,x))
+    p  = lambda x: np.sum([ck[i]*phik[i](x) for i in range(m+1)])
     return p
 
-# funkcja ekstrapolująca
-def extrapolate(p, x):
-    return p(x)
+def approx(m):
+    P = p_gwiazdka(m)
+    return P
 
 # aproksymacja
+x = np.linspace(0, 2, 100)
+y = f(x)
 m = 2
-p = approx(x, y, m)
-y_hat = extrapolate(p, x)
+p = approx(m)
+y_hat = [p(x_) for x_ in x]
+print(p)
+print(y_hat)
 
 # wykres
 plt.plot(x, y, label='f(x) = √x')
